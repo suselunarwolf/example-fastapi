5b19842ec2013af839c994fd1efc11b4e24a52b8
suselunarwolf <suseebymuralii@gmail.com>
2022-07-21 23:16:10 +0530

    changed cascade

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class Post(Base):
     content = Column(String, nullable = False)
     published = Column(Boolean, server_default='True',nullable = False)
     created_at = Column(TIMESTAMP(timezone=True),nullable = False , server_default=text('now()'))
-    owner_id = Column(Integer,ForeignKey("users.id",ondelete=cascade),nullable = False)
+    owner_id = Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),nullable = False)
     owner = relationship('User')
 
 class User(Base):
@@ -32,5 +32,5 @@ class User(Base):
 
 class Vote(Base):
     __tablename__ = "votes"
-    post_id = Column(Integer,ForeignKey("posts.id",ondelete=CASCADE),primary_key=True,nullable=False)
-    user_id = Column(Integer,ForeignKey("users.id",ondelete=CASCADE),primary_key=True,nullable=False)
+    post_id = Column(Integer,ForeignKey("posts.id",ondelete="CASCADE"),primary_key=True,nullable=False)
+    user_id = Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),primary_key=True,nullable=False)
